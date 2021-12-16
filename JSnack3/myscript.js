ec832555f1874b8eb87aa55ab511cd3ae3eea931
Sara-Barbi <barbisara02@gmail.com>
2021-12-16 16:16:29 +0100

    div colorati

---
+++ JSnack3/myscript.js
@@ -0,0 +1,11 @@
+let elemento = document.createElement("div");
+document.body.appendChild(elemento);
+elemento.classList.add("red");
+document.querySelector(".red").innerHTML = "ciao";
+
+
+let elemento2 = document.createElement("div");
+document.body.appendChild(elemento2);
+elemento2.classList.add("green");
+document.querySelector(".green").innerHTML = "ciao";
+
