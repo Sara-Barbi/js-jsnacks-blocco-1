00a88ea2ba1374fe44ef897a8071f3022db18235
Sara-Barbi <barbisara02@gmail.com>
2021-12-16 15:49:11 +0100

    2cicli for

--- JSnack2/myscript.js
+++ JSnack2/myscript.js
@@ -2,7 +2,12 @@ let N = parseInt(prompt("inserisci un numero"));
 
 
 for(let i=0; i<N; i++){
-    let myArr= [(Math.floor(Math.random()*100)+1)]; 
+    let myArr= []; 
     
-    console.log(myArr);
+    for(let index = 0; index <10; index++ ){
+        myArr.push(Math.floor(Math.random()*100)+1);
+
+        console.log(myArr);
+    }
+
 };
