ac02b582fc4e7d549575079516c1b952badacdf7
Sara-Barbi <barbisara02@gmail.com>
2021-12-16 15:06:36 +0100

    aggiunta di i

--- JSnack1/myscript.js
+++ JSnack1/myscript.js
@@ -1,11 +1,16 @@
-myArr = [];
-let myPrompt =parseInt(prompt("inserisci un numero inferiore a 50"));
-myArr.push(myPrompt);
+let myArr = [];
+let somma = 0;
+let i=0;
 
+do {
+    let myPrompt =parseInt(prompt("inserisci un numero inferiore a 50"));
+    myArr.push(myPrompt);
+    somma += myArr[i];
+    i++;
+} while (somma<50);
 
-
-
-
+console.log(myArr);
+console.log(somma);
 
 
 
