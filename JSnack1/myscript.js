66db1d791043773041cfe58aac2c4865e0427269
Sara-Barbi <barbisara02@gmail.com>
2021-12-17 09:53:09 +0100

    better

--- JSnack1/myscript.js
+++ JSnack1/myscript.js
@@ -1,12 +1,10 @@
 let myArr = [];
 let somma = 0;
-let i=0;
 
 do {
     let myPrompt =parseInt(prompt("inserisci un numero inferiore a 50"));
     myArr.push(myPrompt);
-    somma += myArr[i];
-    i++;
+    somma += myPrompt;
 } while (somma<50);
 
 console.log(myArr);
