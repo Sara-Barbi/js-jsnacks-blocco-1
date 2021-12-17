d96a56b77a2850f0f440f9e2868b68c2c8baa5fe
Sara-Barbi <barbisara02@gmail.com>
2021-12-17 15:27:18 +0100

    comments

--- JSnack3/myscript.js
+++ JSnack3/myscript.js
@@ -10,10 +10,10 @@ document.body.appendChild(elemento2);
 
 let myNumbers = [1,2,3,4,5,6,7,8,9];
 
-for(let i=0; i< myNumbers.length; i++ ){
-    if(myNumbers[i]%2 == 0){
-        
-        document.querySelector("#red").innerHTML += myNumbers[i] +" ";
+for(let i=0; i< myNumbers.length; i++ ){ //i è uguale a 0 ma alla fine del ciclo è uguale a 2.
+    if(myNumbers[i]%2 == 0){             //quindi dire myNumbers[i] è come dire tutti i numeri dell'array.
+         
+        document.querySelector("#red").innerHTML += myNumbers[i] +" ";  //inserisvco uno spazio per vedere i numeri distanziati
 
     }else{
         document.querySelector("#green").innerHTML += myNumbers[i] +" ";
